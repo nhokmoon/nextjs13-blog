12f1d6d1b448d126a666df53c7c5cf8ec80b4e52
tuandev <tranminhtuan12an3nh2019@gmail.com>
2023-10-06 12:57:13 +0700

    morning 06/10/2023

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import PaddingContainer from "@/components/layout/padding-container";
+import PostCard from "@/components/post/post_card";
+import { DUMMY_POST } from "@/DUMMY_DATA";
 
 export default function Home() {
   return (
     <PaddingContainer>
-      <main className="h-auto">
-        <h1>Home</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam assumenda suscipit facere
-          odio, unde aut natus adipisci, laborum error nostrum ipsa officia excepturi incidunt
-          laudantium sunt soluta. Quidem, at dolore?
-        </p>
+      <main className="h-auto space-y-10">
+        <PostCard post={DUMMY_POST[0]} />
       </main>
     </PaddingContainer>
   );
